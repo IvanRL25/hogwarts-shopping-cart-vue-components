ca5fd0be6f9c508bed4b4df281a17b1d273f0924
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-03-10 22:24:37 -0500

    addition of component events

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -3,6 +3,8 @@
     <CartListItem v-for="cartItem in cartItems" 
     :key="cartItem.id"
     :item="cartItem"
+    @item-remove="$emit('item-remove', $event)"
+    @quantity-update="$emit('quantity-update', $event)"
     ></CartListItem>
 
 </div>
@@ -12,6 +14,7 @@
 
 import CartListItem from '@/components/CartListItem.vue'
 defineProps(['cartItems'])
+defineEmits(['item-remove','quantity-update'])
 </script>
 
 <style scoped></style>
